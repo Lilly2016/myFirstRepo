d5ac14e044cde4493ec4f5b885ad245b884f7ea1
lilly2016 <leila.creagh@kakku.co>
2018-02-14 15:19:19 +1100

    Added login object to JS file

--- myJavascript.js
+++ myJavascript.js
@@ -1,5 +1,10 @@
 'use strict;'
 
 module.exports = {
-    
+    login: {
+        username: element(by.css('#username')),
+        password: element(by.css('#password')),
+        loginButton: element(by.css('[type="submit"]'))
+    }
+
 }
