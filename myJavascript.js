db4c4a7991d73bdffeaeee43c165a895c2922199
lilly2016 <leila.creagh@kakku.co>
2018-02-15 11:41:24 +1100

    Changed to double quotations, and added ';'

--- myJavascript.js
+++ myJavascript.js
@@ -1,10 +1,9 @@
-'use strict;'
+'use strict;';
 
 module.exports = {
     login: {
-        username: element(by.css('#username')),
-        password: element(by.css('#password')),
-        loginButton: element(by.css('[type="submit"]'))
+        username: element(by.css("#username")),
+        password: element(by.css("#password")),
+        loginButton: element(by.css("[type='submit']"))
     }
-
-}
+};
