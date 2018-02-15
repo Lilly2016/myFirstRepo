3c82f929ad83c3cf78d38796dfdd3bb34bb8ed2b
lilly2016 <leila.creagh@kakku.co>
2018-02-15 11:55:55 +1100

    Removed deleted file

--- myJavascript.js
+++
@@ -1,9 +0,0 @@
-'use strict;';
-
-module.exports = {
-    login: {
-        username: element(by.css("#username")),
-        password: element(by.css("#password")),
-        loginButton: element(by.css("[type='submit']"))
-    }
-};
